02bf7bdbe782697af778e62b9e055f174fa58fea
Mustafa Gördesli <02484829@kocsistem.onmicrosoft.com>
2020-07-06 15:42:32 +0300

    init

---
+++ JWTSample/Entities/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JWTSample.Entities
+{
+    //DbContext sınıfından kalıtım alarak bunun bir context olacağını belirtiyorum.
+    public class ApplicationDbContext : DbContext
+    {
+        //Veritabanına hazırladığım modeli tablo olarak eklemesini söylüyorum.
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        //Veritabanı olarak SQLite kullanacağımı söylüyorum.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=jwtSample.db");
+
+        //Veritabanı oluşturulurken dummy data ile oluşturulmasını istiyorum.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = 1,
+                FirstName = "test",
+                Username = "testUser",
+                Password = "testPassword"
+            });
+        }
+    }
+}
---
+++ JWTSample/Services/User/UserService.cs
@@ -0,0 +1,72 @@
+﻿using JWTSample.Helpers;
+using JWTSample.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTSample.Services.User
+{
+    //DI için oluşturduğumuz arayüzü implemente ediyoruz
+    public class UserService : IUserService
+    {
+        private readonly AppSettings _appSettings;
+        //private readonly ApplicationDbContext _dbContext;
+        public UserService(IOptions<AppSettings> appSettings
+            //, ApplicationDbContext dbContext
+            )
+        {
+            //_dbContext = dbContext;
+            _appSettings = appSettings.Value;
+        }
+
+        //Ekstra bir DTO veya model oluşturmamak için şimdilik değerlerimi geriye tuple olarak dönüyorum.
+        public (string username, string token)? Authenticate(string username, string password)
+        {
+            //Kullanıcının gerçekten olup olmadığı kontrol ediyorum yoksa direk null dönüyorum.
+            //var user = _dbContext.ApplicationUsers.SingleOrDefault(x => x.Username == username && x.Password == password);
+
+            //if (user == null)
+            //    return null;
+
+            if(username != "mustafa" && password != "password")
+            {
+                return null;
+            }
+
+            // Token oluşturmak için önce JwtSecurityTokenHandler sınıfından instance alıyorum.
+            var tokenHandler = new JwtSecurityTokenHandler();
+            //İmza için gerekli gizli anahtarımı alıyorum.
+            var key = Encoding.ASCII.GetBytes(_appSettings.SecretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                //Özel olarak şu Claimler olsun dersek buraya ekleyebiliriz.
+                Subject = new ClaimsIdentity(new[]
+                {
+                    //İstersek string bir property istersek ClaimsTypes sınıfının sabitlerinden çağırabiliriz.
+                    //new Claim("userId", user.Id.ToString()),
+                    //new Claim(ClaimTypes.Name,user.Username)
+
+                    new Claim("userId", "test"),
+                    new Claim(ClaimTypes.Name,"mustafa")
+                }),
+                //Tokenın hangi tarihe kadar geçerli olacağını ayarlıyoruz.
+                Expires = DateTime.UtcNow.AddMinutes(2),
+                //Son olarak imza için gerekli algoritma ve gizli anahtar bilgisini belirliyoruz.
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            //Token oluşturuyoruz.
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            //Oluşturduğumuz tokenı string olarak bir değişkene atıyoruz.
+            string generatedToken = tokenHandler.WriteToken(token);
+
+            //Sonuçlarımızı tuple olarak dönüyoruz.
+            //return (user.Username, generatedToken);
+            return (username, generatedToken);
+        }
+    }
+}
